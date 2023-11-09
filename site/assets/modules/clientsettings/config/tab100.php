f184b7ad803321a3e2371eeb68796fdd083bca46
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2023-11-09 18:05:06 +0400

    update

--- site/assets/modules/clientsettings/config/tab100.php
+++ site/assets/modules/clientsettings/config/tab100.php
@@ -3,6 +3,16 @@ return [
 	'caption'  => 'SEO',
 	'introtext' => 'Настройки для SEO',
 	'settings' => [
+		'site_description' => [
+			'caption' => 'Приветственное сообщение на сайте',
+			'type'  => 'text',
+			'note'  => '',
+			'default_text' => 'Мы рады видеть Вас на официальном сайте государственного бюджетного общеобразовательного учреждения основной общеобразовательной школы с. Покровка муниципального района Кинельский Самарской области',
+		],
+		'site_photogallery' => [
+			'caption' => 'Карусель станицы',
+			'type'  => 'custom_tv:multitv',
+		],
 		'yandex_counter' => [
 			'caption' => 'ID Yandex счётчика',
 			'type'  => 'text',
